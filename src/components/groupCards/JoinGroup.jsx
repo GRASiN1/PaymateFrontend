c9ffe9d2ccabc0e834cfd6a6734d9ba9e19b73ca
GRASiN1 <gauravrajsingh047@gmail.com>
2025-02-13 20:59:18 +0530

    fixed join group if user is not ready

--- src/components/groupCards/JoinGroup.jsx
+++ src/components/groupCards/JoinGroup.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from "react";
 import { useGroups } from "../../contexts/GroupContext";
 import { useNavigate, useParams } from "react-router-dom";
+import { useUser } from "../../contexts/UserContext";
 
 export default function JoinGroup() {
   const { joinGroup } = useGroups();
   const { groupLink } = useParams();
+  const { user } = useUser();
   const navigate = useNavigate();
   useEffect(() => {
     (async function join() {
-      if (groupLink) {
-        await joinGroup(groupLink);
-        navigate("/groups");
+      if(user){
+        if (groupLink) {
+          await joinGroup(groupLink);
+          navigate("/groups");
+        }
+      }else{
+        navigate("/authenticate");
       }
     })();
   });
