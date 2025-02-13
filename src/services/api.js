567ac1417a3941ef148c54ac31270bdfadfe9627
GRASiN1 <gauravrajsingh047@gmail.com>
2025-02-13 10:21:47 +0530

    changed backend port

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://localhost:3000/api";
 const END_POINTS = {
   GET_USER: "/users/user",
   LOGIN: "/users/login",
