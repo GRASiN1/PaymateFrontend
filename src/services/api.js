58293ed66c9a9412c3b6c31015f360ad6dde1b19
GRASiN1 <gauravrajsingh047@gmail.com>
2025-02-13 14:32:10 +0530

    updated base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://paymatebackend.onrender.com/api";
 const END_POINTS = {
   GET_USER: "/users/user",
   LOGIN: "/users/login",
